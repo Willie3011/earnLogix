f362f3e3f1c3f19c62b78d7c14b31a73c1968d3d
Willem April <90314284+Willie3011@users.noreply.github.com>
2024-03-14 14:39:36 +0200

    Created a validation method for sign in page

--- JS/signin.js
+++ JS/signin.js
@@ -9,3 +9,51 @@ toggle.addEventListener("click", () => {
 signUpBtn.addEventListener("click", () => {
     window.location = "register.html";
 })
+
+const form = document.querySelector("form");
+let person = {};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    validateInputs();
+})
+
+function validateInputs(){
+    
+    //get all inputs
+    const emailEl = document.getElementById("email"),
+    passwordEl = document.getElementById("password");
+
+    //get input values
+    const email = emailEl.value.trim(),
+    password = passwordEl.value.trim();
+
+    // get error paragraph elements
+    const emailError = document.getElementById("email-error"),
+    passwordError = document.getElementById("password-error");
+
+
+    //check if inputs are empty and throw an error
+    if(email === "" || email === null){
+        emailError.textContent = "Email field cannot be empty"
+        emailEl.classList.add("error");
+        emailEl.focus();
+    }
+    else{
+        emailError.textContent = "";
+        emailEl.classList.remove("error");
+        person.email = email;
+    }
+
+    if(password === "" || password === null){
+        passwordError.textContent = "Password field cannot be empty";
+        passwordEl.classList.add("error");
+    }
+    else{
+        passwordError.textContent = "";
+        passwordEl.classList.remove("error");
+        person.password = password;
+        console.log(person);
+    }
+
+}
