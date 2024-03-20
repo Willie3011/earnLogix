7a0aae58220de97b73fd7543ef79f32bb9080e21
Willem April <90314284+Willie3011@users.noreply.github.com>
2024-03-20 09:52:51 +0200

    Media query for sidebar
    - Added a logo and menu button for devices less than 768px
    - removed sidebar for devices less than 768px

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -3,7 +3,18 @@ function toggleDropdown(){
     dropDownContent.classList.toggle("active");
 }
 
+//sidebar toggling
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
 
 btn.addEventListener("click", () => sidebar.classList.toggle("active"));
+
+let headerBtn = document.querySelector("#header-toggle-btn");
+
+headerBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("active")
+    btn.classList.add("bx-x");
+
+    btn.addEventListener("click", () => btn.classList.remove("bx-x"))
+
+});
