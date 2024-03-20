c39d14eb943a158a97dc62471a55181590b04964
Willem April <90314284+Willie3011@users.noreply.github.com>
2024-03-20 18:32:23 +0200

    Created cards for the overview
    - Created cards for overview with progress circles
    - Used session Storage to connect sign in and dashboard

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,3 +1,7 @@
+const username = document.getElementById("username");
+
+username.textContent = sessionStorage.getItem("username");
+
 function toggleDropdown(){
     let dropDownContent = document.querySelector(".dropdownContent");
     dropDownContent.classList.toggle("active");
@@ -18,3 +22,23 @@ headerBtn.addEventListener("click", () => {
     btn.addEventListener("click", () => btn.classList.remove("bx-x"))
 
 });
+
+//Card progress circles
+
+function setProgress(className, value, limit){
+    const progress = document.querySelector(className);
+    const radius = progress.getAttribute("r");
+    const circumference = 2 * Math.PI * radius;
+    const offset = circumference - (value / limit) * circumference;
+    progress.style.strokeDasharray = `${circumference} ${circumference}`;
+    progress.style.strokeDashoffset = offset;
+    console.log(progress)
+
+}
+
+window.onload = () => {
+    setProgress(".p1", 2786, 5000);
+    setProgress(".p2", 26, 31);
+    setProgress(".p3", 9, 15);
+    setProgress(".p4", 120, 200);
+}
